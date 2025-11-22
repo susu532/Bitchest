c4616b9db6092e3aa037d246585af4edbae29f71
mimi489 <nnnrrrtttsss123@gmail.com>
2025-11-22 10:57:45 +0100

    update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -18,13 +18,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.1.231:3000'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Content-Length', 'X-JSON-Response'],
 
     'max_age' => 0,
 
